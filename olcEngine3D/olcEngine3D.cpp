b8f5eb9842fef7792ac645ef1ada6e72a689b863
Lilithe Lotor <lilithelotor@gmail.com>
2019-11-21 21:22:53 -0500

    [Model] Fiddling with artisan world

--- olcEngine3D/olcEngine3D.cpp
+++ olcEngine3D/olcEngine3D.cpp
@@ -656,7 +656,7 @@ public:
 		};
 
 		// Load model
-		meshCube.LoadFromObjectFile("Artisan Home1.obj", true);
+		meshCube.LoadFromObjectFile("Artisan Home2.obj", true);
 
 		// Load a sprite from PNG using lodepng
 		std::vector<unsigned char> png;
@@ -664,7 +664,7 @@ public:
 		unsigned width, height;
 
 		//load and decode
-		unsigned error = lodepng::load_file(png, "artisanstextures16.png");
+		unsigned error = lodepng::load_file(png, "artisanstextures64.png");
 		//unsigned error = lodepng::load_file(png, "circle.png");
 		if (!error) error = lodepng::decode(image, width, height, png);
 
